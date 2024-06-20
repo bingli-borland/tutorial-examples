a09b1b92af3b7abc0e3b9414321b8615900aefbd
bingli-borland <1042996632@qq.com>
2024-06-20 19:18:23 +0800

    增加jdbc资源测试

--- ejb/cart/cart-war1/src/main/java/javaeetutorial/cart/war1/HelloServlet.java
+++ ejb/cart/cart-war1/src/main/java/javaeetutorial/cart/war1/HelloServlet.java
@@ -1,7 +1,8 @@
 package javaeetutorial.cart.war1;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,6 +11,13 @@ import java.io.IOException;
 public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().write("hello cart servlet1");
+        String jndiName = req.getParameter("jndiName") == null ? "java:comp/env/jdbcMysqlRef" : req.getParameter("jndiName");
+        resp.getWriter().write("hello cart servlet1<br>");
+        try {
+            Context context = new InitialContext();
+            resp.getWriter().write( jndiName + " = " + context.lookup(jndiName));
+        } catch (Exception e) {
+            e.printStackTrace(resp.getWriter());
+        }
     }
 }
--- ejb/cart/cart-war2/src/main/java/javaeetutorial/cart/war2/HelloServlet.java
+++ ejb/cart/cart-war2/src/main/java/javaeetutorial/cart/war2/HelloServlet.java
@@ -1,5 +1,7 @@
 package javaeetutorial.cart.war2;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,6 +11,13 @@ import java.io.IOException;
 public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String jndiName = req.getParameter("jndiName") == null ? "java:comp/env/jdbc/jdbcMysqlRef" : req.getParameter("jndiName");
         resp.getWriter().write("hello cart servlet2");
+        try {
+            Context context = new InitialContext();
+            resp.getWriter().write( jndiName + " = " + context.lookup(jndiName));
+        } catch (Exception e) {
+            e.printStackTrace(resp.getWriter());
+        }
     }
 }
